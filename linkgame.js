1b9e8762513f0331f29fbbfa5522eb5990309b3f
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2025-09-20 15:51:46 +0800

    Update linkgame.js

--- linkgame.js
+++ linkgame.js
@@ -2,8 +2,9 @@ function link() {location.replace("#");}
 function link() {window.open("#");}
 function linkdota2() {window.open("https://www.dota2.com/home");}
 function linkmobilelegend() {window.open("https://www.mobilelegends.com/");}
-function linkleagueoflegend() {window.open("https://www.leagueoflegends.com/en-gb/");}
+function linkleagueoflegends() {window.open("https://www.leagueoflegends.com/en-gb/");}
 function linkvalorant() {window.open("https://playvalorant.com/en-us/");}
 function linkcounterstrike2() {window.open("https://www.counter-strike.net/cs2");}
+
 
 
