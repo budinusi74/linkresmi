a2ae273ab5fcb6a07c87665878eb4abe7dce859d
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2025-09-20 14:26:10 +0800

    Update linkgame.js

--- linkgame.js
+++ linkgame.js
@@ -1,4 +1,5 @@
 function link() {location.replace("#");}
 function link() {window.open("#");}
-function linkdewatogel() {window.open("#");}
-function linktokekwin() {window.open("#");}
+function linkdota2() {window.open("https://www.dota2.com/home");}
+function linkmobilelegend() {window.open("https://www.mobilelegends.com/");}
+
