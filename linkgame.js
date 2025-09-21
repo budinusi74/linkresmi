194e56f787db659ac9f8464ce2d94a01d1d454b8
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2025-09-21 12:56:33 +0800

    Update linkgame.js

--- linkgame.js
+++ linkgame.js
@@ -5,6 +5,8 @@ function linkmobilelegend() {window.open("https://www.mobilelegends.com/");}
 function linkleagueoflegends() {window.open("https://www.leagueoflegends.com/en-gb/");}
 function linkvalorant() {window.open("https://playvalorant.com/en-us/");}
 function linkcounterstrike2() {window.open("https://www.counter-strike.net/cs2");}
+function linkkingofglory() {window.open("https://king-of-glory.en.filerox.com/android");}
+
 
 
 
