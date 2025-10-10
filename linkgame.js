750a98a8c8476cdfd94abd23f25f1313c95a35ed
budinusi74 <151961031+budinusi74@users.noreply.github.com>
2025-10-10 22:52:00 +0800

    Update linkgame.js

--- linkgame.js
+++ linkgame.js
@@ -8,8 +8,9 @@ function linkcounterstrike2() {window.open("https://www.counter-strike.net/cs2")
 function linkkingofglory() {window.open("https://king-of-glory.en.filerox.com/android");}
 function linkrothelostmemories() {window.open("https://ragnarok-the-lost-memories.en.uptodown.com/android");}
 function linkroblox() {window.open("https://www.roblox.com/");}
-function linkqq288() {window.open("https://qq288qsc.com/");}
 function linkslclashofspirimasters() {window.open("https://play.google.com/pc-store/games/details?id=com.global.dl3dvn&hl=en");}
+function linkqq288() {window.open("https://qq288qsc.com/");}
+
 
 
 
